bbb679aced2aaf83bb639af53f6bca5500de1b18
oddcoder <ahmedsoliman@oddcoder.com>
2019-03-23 23:01:07 +0200

    Now we can open a file

--- cutterplugin.py
+++ cutterplugin.py
@@ -1,6 +1,6 @@
 import cutter
 from PySide2 import QtCore
-from PySide2.QtWidgets import QAction, QStackedWidget 
+from PySide2.QtWidgets import QAction, QFileDialog
 from cutterdrcov.autogen import Ui_DockWidget
 from cutterdrcov.drcov import DCov_load
 from cutterdrcov.covTable import *
@@ -11,21 +11,27 @@ class MyDockWidget(cutter.CutterDockWidget, Ui_DockWidget):
         self.setupUi(self)
         self.stackedWidget.setCurrentIndex(0)
         self.loader.dragEnterEvent = self.loaderDragEnterEvent
-        self.loader.dropEvent = self.loaderDropEvent
+        self.loader.dropEvent = self.dropFile
         self.loader.setAcceptDrops(True)
+        self.loader.mousePressEvent = self.openFile
     def loaderDragEnterEvent(self, e):
         if e.mimeData().hasUrls():
             self.loader.setCursor(QtCore.Qt.DragMoveCursor)
             e.acceptProposedAction()
 
-    def loaderDropEvent(self, e):
+    def dropFile(self, e):
         self.loader.setCursor(QtCore.Qt.PointingHandCursor)
         files = e.mimeData().urls()
-        file_name = files[0].toLocalFile()
+        fileName = files[0].toLocalFile()
+        self.loadcov(fileName)
+    def openFile(self, e):
+        fileName = QFileDialog.getOpenFileName(self)
+        self.loadcov(fileName[0])
+
+    def loadcov(self, file_name):
         modules, bbs = DCov_load(file_name)
         self.stackedWidget.setCurrentIndex(1)
         initCovTable(self.covTable, modules, bbs)
-
 class CutterCovPlugin(cutter.CutterPlugin):
     name = "CutterCov"
     description = "Visualize DynamoRIOCov data into Cutter"
