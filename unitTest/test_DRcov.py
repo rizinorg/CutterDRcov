fc77dc1c1a4838567a2ec881d08f45eccb68d560
oddcoder <ahmedsoliman@oddcoder.com>
2019-03-25 19:07:04 +0200

    Add test for drcov 2.2 (DynamoRIO 7.0.0-RC1)

--- unitTest/test_DRcov.py
+++ unitTest/test_DRcov.py
@@ -6,15 +6,15 @@ from cutterDRcovPlugin import drcov
 #class drcovTest(unittest.TestCase):
 #    def 
 class TestDRcov(unittest.TestCase):
+
     def test_get_file_size(self):
         f = open("test_files/drcov1.log", "r")
         size = drcov.get_file_size(f)
         f.close()
         self.assertEqual(size, 851)
 
-    #2_4 stands for Version 2 Module version 4
-    def test_drcov_2_4_linux(self):
-        modules, bbs = drcov.load("test_files/drcov1.log")
+    def verify_test1_asm(self, covfile):
+        modules, bbs = drcov.load(covfile)
         self.assertEqual(len(modules), 1)
         self.assertEqual(modules[0]['start'],0x8048000)
         self.assertEqual(modules[0]['name'], 'test1.bin')
@@ -24,5 +24,20 @@ class TestDRcov(unittest.TestCase):
         self.assertEqual(bbs[0x60], 20)
         self.assertEqual(bbs[0x74], 7)
         self.assertEqual(bbs[0x8a], 12)
+
+    def test_drcov_2_4_linux(self):
+        # DRCOV VERSION: 2
+        # Module Table: version 4
+        # dynamoRIO 7.1.0-1 linux 32
+        # test1.asm
+        self.verify_test1_asm("test_files/drcov1.log")
+
+    def test_drcov_2_2_linux(self):
+        # DRCOV VERSION: 2
+        # Module Table: version 2
+        #dynamoRIO 7.0.0-RC1 linux 32
+        # test1.asm
+        self.verify_test1_asm("test_files/drcov2.log")
+
 if __name__ == '__main__':
     unittest.main()
