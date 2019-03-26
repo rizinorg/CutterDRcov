7f35c049a1bcb18e4e1358732d87a5ddf28ff0f2
oddcoder <ahmedsoliman@oddcoder.com>
2019-03-27 01:45:43 +0200

    Add unit test to test uncovered functions

---
+++ test_files/test2.asm
@@ -0,0 +1,28 @@
+SECTION .text
+	GLOBAL _start 
+	GLOBAL uncalled
+
+uncalled:
+	push ebp
+	mov ebp, esp
+	mov eax, 1
+	pop ebp
+	ret
+
+_start:
+	mov eax, 5
+	mov ebx, 6
+	mov ecx, 7
+	cmp ecx, 6
+	je BB2
+	mov eax, 7
+	jmp BB3
+
+BB2:	mov eax, 5
+	mov ebx, 6
+	mov ecx, 7
+
+BB3:	mov eax,1            ; 'exit' system call
+	mov ebx,0            ; exit with error code 0
+	int 80h              ; call the kernel
+	call uncalled
--- unittest/test_covtable.py
+++ unittest/test_covtable.py
@@ -15,7 +15,7 @@ def test_1_cmdj(cmd):
             'bin': {'baddr': 0x8048000}
         }
     if cmd == "aflj":
-        return [{'offset': 134512736, 'name': 'entry0'}]
+        return [{'offset': 0x8048060, 'name': 'entry0'}]
     if cmd == "afbj @entry0":
         return [
             {'addr': 0x8048060, 'size': 20, 'ninstr': 5},
@@ -24,6 +24,31 @@ def test_1_cmdj(cmd):
             {'addr': 0x804808a, 'size': 12, 'ninstr': 3}
         ]
     return None
+
+def test_2_cmdj(cmd):
+    if cmd == "ij":
+        return {
+            'core': {'file': 'test2.bin'},
+            'bin': {'baddr': 0x8048000}
+        }
+    if cmd == "aflj":
+        return [
+            {'offset': 0x8048060, 'name': 'loc.uncalled'},
+            {'offset': 0x804806a, 'name': 'entry0'}
+            ]
+    if cmd == "afbj @entry0":
+        return [
+            {'addr': 0x804806a, 'size': 20, 'ninstr': 5},
+            {'addr': 0x804807e, 'size': 7, 'ninstr': 2},
+            {'addr': 0x8048085, 'size': 15, 'ninstr': 3},
+            {'addr': 0x8048094, 'size': 17, 'ninstr': 4}
+        ]
+    if cmd == "afbj @loc.uncalled":
+        return [
+            {"addr": 0x8048060, "size": 10, "ninstr": 5}
+        ]
+    return None
+
 
 
 import cutter
@@ -45,17 +70,33 @@ class TestcovTable(unittest.TestCase):
         self.assertEqual(entry, hardcoded_entry)
         self.assertEqual(hits, {0x8048060, 0x804808a, 0x8048074})
 
-    @patch("cutter.cmdj", side_effect=test_1_cmdj)
-    def test_analyse(self, _):
-        modules, bbs = drcov.load("test_files/drcov2.4.log")
+    def do_analyse(self, modules, bbs, table, hits):
         config = {}
         config['modules'] = modules
         config['bbs'] = bbs
         covtable.analyse(config)
-        table = [['76.923%', 'entry0', '0x08048060', '10/13', '3/4']]
-        hits = {0x8048060, 0x804808a, 0x8048074}
         self.assertEqual(config['table'], table)
         self.assertEqual(config['bb_hits'], hits)
+
+    @patch("cutter.cmdj", side_effect=test_1_cmdj)
+    def test_analyse1(self, _):
+        modules, bbs = drcov.load("test_files/drcov2.4.log")
+        table = [['76.923%', 'entry0', '0x08048060', '10/13', '3/4']]
+        hits = {0x8048060, 0x804808a, 0x8048074}
+        self.do_analyse(modules, bbs, table, hits)
+
+    @patch("cutter.cmdj", side_effect=test_2_cmdj)
+    def test_analyse2(self, _):
+        """
+        This test case where not all functions gets covered,
+        so they needed be reported
+        """
+        modules, bbs = drcov.load("test_files/drcov.test2.log")
+        # technically speaking only 10 instructions can get executed but what
+        # can I do ... any way it wouldn't matter
+        table = [["78.571%", "entry0", "0x0804806a", "11/14", "3/4"]]
+        hits = {0x0804806a, 0x0804807e, 0x08048094}
+        self.do_analyse(modules, bbs, table, hits)
 
 if __name__ == '__main__':
     unittest.main()
